7ebd16a8f0d49614abd65f10fad445059f926a25
Steven Sills II <stevensills2@gmail.com>
2025-12-01 21:39:15 -0800

    4th cleanup

--- backend/cleanup_and_migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Cleaning up partial V12 migration..."
-export PGPASSWORD=church_password
-psql -h localhost -p 5433 -U church_user -d church_app -f cleanup_v12.sql
-echo ""
-echo "Cleanup completed. Now running Flyway migration..."
-echo ""
-./mvnw flyway:migrate
